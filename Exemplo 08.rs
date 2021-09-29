0a05f1e38b3c9ee60e4dac2a19a9ea18c67803da
Matheus-Alfaia <91610761+Matheus-Alfaia@users.noreply.github.com>
2021-09-29 15:33:53 -0400

    Update Exemplo 08.rs

--- Exemplo 08.rs
+++ Exemplo 08.rs
@@ -1,11 +1,13 @@
 fn main() {
+    //declaracação das variaveis de apoio
     let a:u16 = 0x6db7;
     let b:u16 = 0x37;
     let m:u16 = 0x3F;
 
-
+    //declaração da variavel de resultado
     let mut result:u16;
     
+    //resultado mostrando o A,a máscara 1 e o resultado da operação a & 0x3F
     result= a;
     println!(" -a = {:016b} =  0x{:x} ",a, result);
 

6c7efa727f83dd5297e9f6eda390ec52803597c1
CheilaMarqs <91612535+CheilaMarqs@users.noreply.github.com>
2021-09-29 16:06:03 -0400

    Update Exemplo 03.rs

--- Exemplo 03.rs
+++ Exemplo 03.rs
@@ -1,1 +1,10 @@
-teu código aqui, bode
+//Exemplo 3
+// a Possui o valor 0x6DB7 e este código mostra qual
+//o valor de ~a que neste caso é 0x9248 
+fn main() { 
+   let a:u16 = 0x6DB7;
+   let result:u16;
+   result=!a;
+   println!("a = {:16b} = 0x{:x} ",a,a);
+   println!("~a = {:16b} = 0x{:x} ",result,result);
+}

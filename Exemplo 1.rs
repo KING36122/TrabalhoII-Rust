6d4e4b0b8b94d6a4130b6a0ecc7ca5203851f4a8
THIAGO RODRIGUES DOS SANTOS <88813390+TRodrigues21@users.noreply.github.com>
2021-09-29 11:34:03 -0300

    Update Exemplo 1.rs

--- Exemplo 1.rs
+++ Exemplo 1.rs
@@ -15,9 +15,9 @@ fn main() {
    // ~0x7FF
    result = !a; // Complemento de a
    // dados de saidas com resultados
-   println!("Representação em bits de 0x7FF = {:16b}", a);
+   println!("Representação em bits de 0x7FF = {:016b}", a);
    println!("Complemento de 0x7FF = 0x{:x}", result);
-   println!("Representação em bits de 0x{:x} = {:16b}",result, result);
+   println!("Representação em bits de 0x{:x} = {:016b\n\n}",result, result);
    
    
    // Operador Complementar "~" com 32 bits
@@ -25,28 +25,28 @@ fn main() {
    // ~0xC5 
    result1 = !b; // Complemento de b
    // dados de saidas com resultados
-   println!("Representação em bits de 0xC5 = {:32b}", b);
+   println!("Representação em bits de 0xC5 = {:032b}", b);
    println!("Complemento de 0xC5 = 0x{:x}", result1);
-   println!("Representação em bits de 0x{:x} = {:32b}",result1, result1);
+   println!("Representação em bits de 0x{:x} = {:032b\n\n}",result1, result1);
    
    // ~0x1111
    result1 = !c; // Complemento de c
    // dados de saidas com resultados
-   println!("Representação em bits de 0x1111 = {:32b}", c);
+   println!("Representação em bits de 0x1111 = {:032b}", c);
    println!("Complemento de 0x1111 = 0x{:x}", result1);
-   println!("Representação em bits de 0x{:x} = {:32b}",result1, result1);
+   println!("Representação em bits de 0x{:x} = {:032b\n\n}",result1, result1);
    
    // 0xFFFF
    result1 = !d; // Complemento de d
    // dados de saidas com resultados
-   println!("Representação em bits de 0xFFFF = {:32b}", d);
+   println!("Representação em bits de 0xFFFF = {:032b}", d);
    println!("Complemento de 0xFFFF = 0x{:x}", result1);
-   println!("Representação em bits de 0x{:x} = {:32b}",result1, result1);
+   println!("Representação em bits de 0x{:x} = {:032b\n\n}",result1, result1);
    
    // ~0x5B3C
    result1 = !e; // Complemento de e
    // dados de saidas com resultados
-   println!("Representação em bits de 0x5B3C = {:32b}", e);
+   println!("Representação em bits de 0x5B3C = {:032b}", e);
    println!("Complemento de 0x5B3C = 0x{:x}", result1);
-   println!("Representação em bits de 0x{:x} = {:32b}",result1, result1);
+   println!("Representação em bits de 0x{:x} = {:032b\n\n}",result1, result1);
 }

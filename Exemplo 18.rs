f717ba528d43b9de58649fe873c364f9679beafb
Gabriel Ferreira <84155888+KING36122@users.noreply.github.com>
2021-09-29 10:52:17 -0400

    Create Exemplo 18.rs

---
+++ Exemplo 18.rs
@@ -0,0 +1,35 @@
+fn main(){
+
+	//declaração da variável
+	let x:u8 = 128;
+
+	//declaração da variável auxiliar
+	let mut aux:u8;
+
+	//print do resultado do deslocamento dos bits um por um
+	println!("Resultado do deslocamento dos bits:");
+
+	aux = x >> 0;
+	println!("{:08b}",aux);
+
+	aux = x >> 1;
+	println!("{:08b}",aux);
+
+	aux = x >> 2;
+	println!("{:08b}",aux);
+
+	aux = x >> 3;
+	println!("{:08b}",aux);
+
+	aux = x >> 4;
+	println!("{:08b}",aux);
+
+	aux = x >> 5;
+	println!("{:08b}",aux);
+
+	aux = x >> 6;
+	println!("{:08b}",aux);
+
+	aux = x >> 7;
+	println!("{:08b}",aux);
+}

8a7c272dbf903830c07e1e696e56cbda274852fe
Matheus-Alfaia <91610761+Matheus-Alfaia@users.noreply.github.com>
2021-09-29 16:27:29 -0400

    🕸 Exemplo 14 - Matheus

--- Exemplo 14.rs
+++ Exemplo 14.rs
@@ -1,1 +1,27 @@
+fn main() {
+    //declaração das variaveis de apoio
+    let a:u16 = 0x6db7;
+    let m:u16 = 0x4;
 
+
+    //declaração da variavel de resultado
+    let mut result:u16;
+    
+    //resultado mostrando o A,a máscara 1 e o resultado da operação a ^0x4
+    result= a;
+    println!(" -a = {:016b} =  0x{:x} ",a, result);
+
+    
+    result = m;
+    println!(" -m = {:016b} =  0x{:x} ",m, result);
+    
+    
+    result = a ^ 0x4;
+    println!(" -b = {:016b} =  0x{:x} ",result, result);
+    
+    result = m;
+    println!(" -m = {:016b} =  0x{:x} ",m, result);
+    
+    result =  (a^ 0x4)^0x4;
+    println!(" -c = {:016b} =  0x{:x} ",result, result);
+}

e9ed9119f6eab7a9403ac224b04a47f530aca1a8
CheilaMarqs <91612535+CheilaMarqs@users.noreply.github.com>
2021-09-29 16:37:46 -0400

    🍬 Exemplo 7 - Cheila

--- Exemplo 07.rs
+++ Exemplo 07.rs
@@ -1,1 +1,12 @@
-teu código
+//Exemplo 7
+// a Possui o valor 0x6DB7 e b Possui o valor 0xA726,este código mostra qual
+//o valor de a XOR b que neste caso é 0xEFB7
+fn main() {
+    let a:u16 = 0x6DB7;
+   let b:u16 = 0xA726;
+   let result:u16;
+   result=a | b;
+   println!("a = {:016b} = 0x{:x} ",a,a);
+   println!("b = {:016b} = 0x{:x} ",b,b);
+   println!("a | b = {:016b} = 0x{:x} ",result,result);
+}

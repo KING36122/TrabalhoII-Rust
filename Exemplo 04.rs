25cf0de2585c2d27e64876c20507e5963d95290e
CheilaMarqs <91612535+CheilaMarqs@users.noreply.github.com>
2021-09-29 16:07:57 -0400

    Update Exemplo 04.rs

--- Exemplo 04.rs
+++ Exemplo 04.rs
@@ -1,1 +1,11 @@
-teu código
+
+//Exemplo 4
+// b Possui o valor 0xA726 e este código mostra qual
+//o valor de ~b que neste caso é 0x58D9
+fn main() { 
+   let b:u16 = 0xA726;
+   let result:u16;
+   result=!b;
+   println!("b = {:16b} = 0x{:x} ",b,b);
+   println!("~b = {:16b} = 0x{:x} ",result,result);
+}

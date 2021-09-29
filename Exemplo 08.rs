a96a6a0de55dfb1853bf0e2f0ffd764222f976cd
Matheus-Alfaia <91610761+Matheus-Alfaia@users.noreply.github.com>
2021-09-29 13:56:09 -0400

    Update Exemplo 08.rs

--- Exemplo 08.rs
+++ Exemplo 08.rs
@@ -1,1 +1,21 @@
-teu código
+fn main() {
+    let a:u16 = 0x6db7;
+    let b:u16 = 0x37;
+    let m:u16 = 0x3F;
+
+
+    let mut result:u16;
+    
+    result= a;
+    println!(" -a = {:016b} =  0x{:x} ",a, result);
+
+    
+    result = m;
+    println!(" -m = {:016b} =  0x{:x} ",m, result);
+    
+    
+    result = a & 0x3F;
+    println!(" -b = {:016b} =  0x{:x} ",b, result);
+
+    
+}

e22ad972cedb68fcca32e29c9780483a8174b5f3
Felipe Vasconcelos Assunção <67162607+assuncaofelipe@users.noreply.github.com>
2021-09-29 18:50:44 -0400

    :snake: Exemplo 19 - Felipe

--- Exemplo 19.rs
+++ Exemplo 19.rs
@@ -1,1 +1,33 @@
+fn main(){
 
+	//declaração da variável
+	let a:u16 = 0x6DC0;
+	
+	//declaração da variável auxiliar
+	let mut b:u16;
+
+	//print do resultado do deslocamento dos bits um por um
+	println!("Resultado do deslocamento dos bits:");
+
+	b = a << 0;
+	println!("{:016b}",b);
+
+	b = a << 1;
+	println!("{:016b}",b);
+
+	b = a << 2;
+	println!("{:016b}",b);
+
+	b = a << 3;
+	println!("{:016b}",b);
+
+	b = a << 4;
+	println!("{:016b}",b);
+
+	b = a << 5;
+	println!("{:016b}",b);
+
+	b = a << 6;
+	println!("{:016b}",b);
+
+}
